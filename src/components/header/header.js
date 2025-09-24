c7d9fdaced43e25f13077bdbb2493070b7f7a57a
han <honahere2020@gmail.com>
2025-09-24 19:25:50 +0300

    367tgdfgdff

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -176,7 +176,7 @@ function Header() {
       </div>
 
       {/* Fourth Part */}
-      <div className="fourth-part flex flex-row items-center justify-center gap-72 w-full px-20  py-2 shadow-md">
+      <div className="fourth-part flex flex-row items-center justify-center  w-full  py-2 shadow-md">
         {/* All Categories */}
         <Menu as="div" className="relative inline-block hidden min-[891px]:block">
           <MenuButton className="inline-flex items-center justify-center gap-x-1 px-4 py-2 text-xs font-medium text-white bg-[#35AFA0] rounded-2xl border-none outline-none">
@@ -193,7 +193,7 @@ function Header() {
         </Menu>
 
         {/* Desktop Links */}
-        <div className="hidden min-[891px]:flex items-center gap-3">
+        <div className="hidden dist min-[891px]:flex items-center gap-2">
           {links.map((link) =>
             link.dropdown ? (
               <Menu as="div" key={link.name} className="relative inline-block">
